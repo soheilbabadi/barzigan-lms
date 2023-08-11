419266898707aef16b2894d05a22f638a0b203e8
babadishyl <babadishyl@gmail.com>
2023-08-12 00:44:19 +0330

    commit untracked

---
+++ barzigan-lms/src/main/java/com/barzigan/www/barziganlms/BarziganLmsApplication.java
@@ -0,0 +1,13 @@
+package com.barzigan.www.barziganlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarziganLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarziganLmsApplication.class, args);
+    }
+
+}
---
+++ barzigan-lms/src/test/java/com/barzigan/www/barziganlms/BarziganLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barzigan.www.barziganlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarziganLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
