8bc846c534df408e752fb2780b36f405c3923dc7
soheil babadi <babadishyl@gmail.com>
2023-08-25 22:03:16 +0330

    remove duplicate PaymentRepository

--- barzigan-lms/src/main/java/com/barzigan/www/barziganlms/payment/infra/InstallmentRepository.java
+++ barzigan-lms/src/main/java/com/barzigan/www/barziganlms/payment/infra/InstallmentRepository.java
@@ -3,10 +3,12 @@ package com.barzigan.www.barziganlms.payment.infra;
 import com.barzigan.www.barziganlms.payment.domain.Installment;
 import com.barzigan.www.barziganlms.payment.domain.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface InstallmentRepository extends JpaRepository<Installment, Long> {
 
     List<Installment> findByPayment(Payment payment);
--- barzigan-lms/src/main/java/com/barzigan/www/barziganlms/payment/infra/PaymentRepository.java
+++ barzigan-lms/src/main/java/com/barzigan/www/barziganlms/payment/infra/PaymentRepository.java
@@ -4,9 +4,11 @@ import com.barzigan.www.barziganlms.course.model.Course;
 import com.barzigan.www.barziganlms.payment.domain.Payment;
 import com.barzigan.www.barziganlms.person.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findAllByCourseAndStudent(Course course, Student student);
 
