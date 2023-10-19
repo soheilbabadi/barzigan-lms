52f26ac3b4d0939c8d9fe375c3ab50d265f148bf
babadishyl <babadishyl@gmail.com>
2023-10-19 17:44:11 +0330

    add navbar

--- barzigan_react/src/App.js
+++ barzigan_react/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './component/Button';
 import Footer from './component/Footer';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
