593fed24edb27892b49b6f4562d97c89de921413
babadishyl <babadishyl@gmail.com>
2023-10-19 17:22:29 +0330

    add font Yekan

---
+++ barzigan_react/src/component/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h3>تست فوتر</h3>
+            <p>کلیه حقوق متعلق به شرکت برزیگان</p>
+        </div>
+    );
+}
+
+export default Footer;
